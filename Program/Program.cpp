86a70c1341a0ca3530f3939625b2997355a4aee0
skek0 <77053845+skek0@users.noreply.github.com>
2024-11-12 21:41:35 +0900

    2024.11.12 단일연결리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class SingleLinkedList
+{
+	struct Node
+	{
+		T data;
+		Node* next;
+	~Node()
+	{
+		cout << "deleted" << endl;		//오류
+	}
+	};
+	int size;
+	Node* head;
+
+public:
+	SingleLinkedList()
+	{
+		size = 0;
+		head = nullptr;
+	}
+	void PushFront(T data)
+	{
+		if (head != nullptr) 
+		{
+			Node* newNode = new Node;
+			newNode->data = data;
+			newNode->next = head;
+			head = newNode;
+			size++;
+		}
+		else
+		{
+			head = new Node;
+			head->data = data;
+			head->next = nullptr;
+			size++;
+		}
+	}
+
+	~SingleLinkedList()
+	{
+		Node* curr = head;
+		Node* prev = new Node;
+		while (curr->next != nullptr)
+		{
+			prev = curr;
+			curr = curr->next;
+			delete prev;
+		}
+		delete head;
+	}
+};
+
+int main()
+{
+	SingleLinkedList<int> singleLinkedList;
+	singleLinkedList.PushFront(10);
+	singleLinkedList.PushFront(20);
+	
+
+	return 0;
+}
