085532780ea9f3f3bedff515d6f273363675b94e
skek0 <77053845+skek0@users.noreply.github.com>
2024-11-29 21:44:02 +0900

    2024.11.29 해시테이블 2

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -52,7 +52,6 @@ public:
 
 		return newNode;
 	}
-
 	void Insert(KEY key, VALUE value)
 	{
 		int _key = HashFunction(key);
@@ -61,23 +60,96 @@ public:
 		if (bucket[_key].head != nullptr)
 		{
 			newNode->next = bucket[_key].head;
-			bucket[_key].head = newNode;
 		}
-		else // bucket[_key].head == nullptr
-		{
-			bucket[_key].head = newNode;
-		}
+		bucket[_key].head = newNode;
 		bucket[_key].count++;
+	}
+	void Print()
+	{
+		Node* currentNode;
+		for (int i = 0; i < SIZE; i++)
+		{
+			currentNode = bucket[i].head;
+
+			cout <<	"[" << i << "]" ;
+
+			while (currentNode != nullptr)
+			{
+				cout << currentNode->key << " " << currentNode->value;
+				currentNode = currentNode->next;
+				if (currentNode != nullptr) cout << "->";
+			}
+			cout << endl;
+		}
+	}
+	void Remove(KEY key)
+	{
+		int _key = HashFunction(key);
+		if (bucket[_key].head != nullptr)
+		{
+			Node* currentNode = bucket[_key].head;
+			Node* previousNode = nullptr;
+
+			while (currentNode->key != key)
+			{
+				previousNode = currentNode;
+				currentNode = currentNode->next;
+				if (currentNode == nullptr)
+				{
+					cout << "KEY NOT FOUND" << endl;
+					return;
+				}
+			}
+			previousNode->next = currentNode->next;
+			currentNode->next = nullptr;
+			delete currentNode;
+			bucket[_key].count--;
+		}
+		else //bucket[HashedKey] == nullptr)
+		{
+			cout << "KEY NOT FOUND" << endl;
+			return;
+		}
+	}
+
+	~HashTable()
+	{
+		Node* deleteNode;
+		Node* nextNode;
+		for (int i = 0; i < SIZE; i++)
+		{
+			if (bucket[i].head != nullptr)
+			{
+				deleteNode = bucket[i].head;
+				nextNode = bucket[i].head;
+
+				while(nextNode != nullptr)
+				{
+					nextNode = nextNode->next;
+					delete deleteNode;
+					deleteNode = nextNode;
+				}
+			}
+		}
 	}
 };
 
 int main()
 {
-	HashTable<int, int> table;
+	HashTable<int, std::string> table;
 
-	table.Insert(3, 10);
-	table.Insert(3, 20);
-	table.Insert(3, 30);
+	table.Insert(10, "ten");
+	table.Insert(20, "twenty");
+	table.Insert(30, "thirty");
+	table.Insert(40, "forty");
+
+	table.Print();
+
+	table.Remove(2);
+	table.Remove(20);
+	table.Remove(20);
+
+	table.Print();
 
 	return 0;
 }
